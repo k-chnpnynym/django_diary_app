621aa0ac65824e5bf51b0f4f128c233c1fd71e51
k-kawano <k.hayashi8001@gmail.com>
2023-01-29 07:59:43 +0900

    日記アプリを作成し、認証機能を追加した。

---
+++ mydiaryproject/diary/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'diary'
+urlpatterns = [
+    path('index/', views.IndexView.as_view(), name='index'),
+    path('diary_create/', views.DiaryCreateView.as_view(), name='diary_create'),
+    path('diary_create_complete/', views.DiaryCreateCompleteView.as_view(), name='diary_create_complete'),
+    path('diary_list/', views.DiaryListView.as_view(), name='diary_list'),  # 追記
+    path('diary_detail/<uuid:pk>/', views.DiaryDetailView.as_view(), name='diary_detail'),  # 追記
+    path('diary_update/<uuid:pk>/', views.DiaryUpdateView.as_view(), name='diary_update'),  # 追記
+    path('diary_delete/<uuid:pk>/', views.DiaryDeleteView.as_view(), name='diary_delete'),  # 追加
+]
