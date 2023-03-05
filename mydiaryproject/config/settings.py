d4002f55c89acfff1433114df9ffe0318673b3e4
k-kawano <k.hayashi8001@gmail.com>
2023-03-06 00:51:33 +0900

    mediaフォルダがmydiaryproject直下に作られるように修正した。

--- mydiaryproject/config/settings.py
+++ mydiaryproject/config/settings.py
@@ -210,4 +210,4 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # MEDIA settings #
 ##################
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR
