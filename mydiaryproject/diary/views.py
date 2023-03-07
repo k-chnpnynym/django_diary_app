5ca035041d23e253b08a3e6330944f104656e055
k-kawano <k.hayashi8001@gmail.com>
2023-03-07 21:20:02 +0900

    diary.views.pyを修正し、内緒フィールドが表示されるようにした。

--- mydiaryproject/diary/views.py
+++ mydiaryproject/diary/views.py
@@ -33,7 +33,7 @@ class DiaryDetailView(DetailView):
 class DiaryUpdateView(UpdateView):
     template_name = 'diary_update.html'
     model = Diary
-    fields = ('date', 'title', 'text', 'image')
+    fields = ('date', 'title', 'text', 'image', 'secret')
     success_url = reverse_lazy('diary:diary_list')
 
     def form_valid(self, form):
